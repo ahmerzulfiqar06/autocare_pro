c54abbd32018510b68d333943965f93682df918e
Nftsworld007 <ahmerparacha2016@gmail.com>
2025-09-23 19:18:44 +0500

    New updates

--- android/build.gradle.kts
+++ android/build.gradle.kts
@@ -6,8 +6,8 @@ allprojects {
 }
 
 plugins {
-    id("com.android.application") version "8.13.0" apply false
-    id("org.jetbrains.kotlin.android") version "1.9.22" apply false
+    id("com.android.application") version "8.2.2" apply false
+    id("org.jetbrains.kotlin.android") version "1.8.10" apply false
 }
 
 val newBuildDir: Directory = rootProject.layout.buildDirectory.dir("../../build").get()

9460e9cf7125af99ffed618b2cb009d39c475255
Nftsworld007 <ahmerparacha2016@gmail.com>
2025-09-23 18:07:14 +0500

    New updates

--- android/build.gradle.kts
+++ android/build.gradle.kts
@@ -6,11 +6,9 @@ allprojects {
 }
 
 plugins {
-  // ...
-
-  // Add the dependency for the Google services Gradle plugin
-  id("com.google.gms.google-services") version "4.4.3" apply false
-
+    id("com.android.application") version "8.1.4" apply false
+    id("org.jetbrains.kotlin.android") version "1.8.10" apply false
+    id("com.google.gms.google-services") version "4.4.3" apply false
 }
 
 val newBuildDir: Directory = rootProject.layout.buildDirectory.dir("../../build").get()

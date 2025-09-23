d589306a3fe0bfda7e06c9284b584887438546ee
Nftsworld007 <ahmerparacha2016@gmail.com>
2025-09-23 18:37:19 +0500

    New updates

--- android/build.gradle.kts
+++ android/build.gradle.kts
@@ -6,8 +6,8 @@ allprojects {
 }
 
 plugins {
-    id("com.android.application") version "8.1.4" apply false
-    id("org.jetbrains.kotlin.android") version "1.8.10" apply false
+    id("com.android.application") version "8.5.0" apply false
+    id("org.jetbrains.kotlin.android") version "1.9.22" apply false
     id("com.google.gms.google-services") version "4.4.3" apply false
 }
 
